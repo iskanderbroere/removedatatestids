dda56ce14d78f5c98708a50e25c884552db4cd55
iskanderbroere <iskanderbroere@gmail.com>
2019-03-15 09:43:25 +0100

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(
+      config,
+      {
+        isDev,
+        loaders: { vue }
+      }
+    ) {
+      vue.compilerOptions = {
+        modules: [
+          {
+            // strip data-test ids on elements in prod
+            // this currently bugs out for the client bundle
+            preTransformNode(astEl) {
+              if (!isDev) {
+                const { attrsMap, attrsList } = astEl
+                const tagAttributesForTesting = ['test', 'test-id']
+                tagAttributesForTesting.forEach(attribute => {
+                  const dataAttr = `data-${attribute}`
+                  if (attrsMap[dataAttr]) {
+                    delete attrsMap[dataAttr]
+                    const index = attrsList.findIndex(x => x.name === dataAttr)
+                    attrsList.splice(index, 1)
+                  }
+                })
+              }
+              return astEl
+            }
+          }
+        ]
+      }
+    }
+  }
+}
